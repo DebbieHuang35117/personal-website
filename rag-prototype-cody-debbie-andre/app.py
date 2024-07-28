2a051a4855bfb209163395b0dc3c070baf9e75ea
andre.liang <namwoam@gmail.com>
2024-07-28 04:12:50 +0100

    add: add data to query

--- rag-prototype-cody-debbie-andre/app.py
+++ rag-prototype-cody-debbie-andre/app.py
@@ -6,22 +6,16 @@ from langchain_openai import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain_openai import OpenAIEmbeddings
 
-file_path = '.api-key/pinecone_api.txt'
-with open(file_path, 'r') as file:
-    PINECONE_API_KEY = file.read().strip()
-os.environ['PINECONE_API_KEY'] = PINECONE_API_KEY
+from dotenv import load_dotenv
 
-file_path = '.api-key/open_ai_api.txt'
-with open(file_path, 'r') as file:
-    OPENAI_API_KEY = file.read().strip()
-os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
+load_dotenv()
 
 async def get_answer_multilingual_e5(query: str) -> str:
     embeddings = PineconeEmbeddings(model="multilingual-e5-large")
     index_name = "sinica-rag-test-0730-multilingual-e5-large"
     vectorstore = PineconeVectorStore(index_name=index_name, embedding=embeddings)
     docs = await asyncio.to_thread(vectorstore.similarity_search, query=query, k=1)
-    llm = OpenAI(api_key=os.environ['OPENAI_API_KEY'], temperature=0.0)
+    llm = OpenAI(api_key=os.environ['OPENAI_API_KEY'], temperature=0.0,model="gpt-4o")
     chain = load_qa_chain(llm, chain_type="map_reduce")
     answer = await asyncio.to_thread(chain.run, input_documents=docs, question=query)
     return answer
---
+++ rag-prototype-cody-debbie-andre/readme.md
@@ -0,0 +1,30 @@
+# Rag-Prototype
+
+This is a demonstration proof-of-concept for the sinica-medLLM project
+
+## API KEY(s)
+
+You should have a `.env` file with the following fields:
+
+```
+PINECONE_API_KEY=<YOUR_API_KEY>
+OPENAI_API_KEY=<YOUR_API_KEY>
+```
+
+## Setup
+
+To store the embedding data to Pinecone database, you must create the following index(es) with the following name and dimension:
+
+| name                                        | dimension | 
+|---------------------------------------------|-----------|
+| sinica-rag-test-0730-text-embedding-3-large | 3072      |
+| sinica-rag-test-0730-text-embedding-3-small | 1536      |
+| sinica-rag-test-0730-multilingual-e5-large  | 1024      |
+
+## Data
+
+The file `taiwan.txt` is generated from this [wiki page](https://zh.wikipedia.org/wiki/%E8%87%BA%E7%81%A3) with [WikiText](https://wikitext.eluni.co/)
+
+## Run the app
+
+To host the streamlit chat app locally, run `streamlit run ./app.py `
