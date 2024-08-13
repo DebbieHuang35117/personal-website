6375fc80a5eb81c8c26800d620df433808a63746
andre.liang <namwoam@gmail.com>
2024-08-13 01:01:49 +0100

    add: add evaluation notebook

--- eval.py
+++ eval.py
@@ -27,27 +27,3 @@ def compute_faithfulness_score(context: List[List[str]], answer: List[str], ques
     df_score = score.to_pandas()
     
     return df_score
-'''
-# Example usage
-data = {
-    'question': ['When was the first super bowl?', 'Who won the most super bowls?'],
-    'answer': ['The first superbowl was held on Jan 15, 1967', 'The most super bowls have been won by The New England Patriots'],
-    'contexts': [['The First AFL–NFL World Championship Game was an American football game played on January 15, 1967, at the Los Angeles Memorial Coliseum in Los Angeles,'], 
-                 ['The Green Bay Packers...Green Bay, Wisconsin.','The Packers compete...Football Conference']],
-    'ground_truth': ['The first superbowl was held on January 15, 1967', 'The New England Patriots have won the Super Bowl a record six times']
-}
-
-# Call the function
-df_score = compute_context_recall_score(data['contexts'], data['ground_truth'], data['question'])
-
-# Print the results
-print(df_score)
-
-df_score = compute_context_recall_score(data['contexts'], data['ground_truth'], [''])
-
-# Print the results
-print(df_score)
-
-# Print the results
-print(df_score)
-'''
