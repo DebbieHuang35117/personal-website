2173c0aa01878d6fa03d806987af4f9725c6940d
andre.liang <namwoam@gmail.com>
2024-10-21 18:42:51 +0800

    chore: minor fixes

--- Makefile
+++ Makefile
@@ -1,10 +1,24 @@
 .PHONY: all build dev stop
 all: build dev
+
+UNAME := $(shell uname)
+
+
+
 build:
-	docker build -t pytorch-mps . -f Dockerfile
+ifeq ($(UNAME), Linux)
+	docker build -t pytorch-cuda . -f Dockerfile.cuda
+else
+	docker build -t pytorch-mps . -f Dockerfile.mps
+endif
 dev:
+ifeq ($(UNAME), Linux)
+	docker run --name pytorch-container -it --rm -v $(shell pwd):/app -p 8501:8501 pytorch-cuda
+else
 	docker run --name pytorch-container -it --rm -v $(shell pwd):/app -p 8501:8501 pytorch-mps 
+endif
 stop:
 	docker kill pytorch-container
 clean:
 	docker rmi pytorch-mps
+	docker rmi pytorch-cuda
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 # custom code for the streamlit app
-from local_hf import reader
 from taide_chat import taide_llm
 
 # dependencies for streamlit and langchain
@@ -22,17 +21,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-openai_llm = ChatOpenAI(
-    model="gpt-4o-mini",
-    temperature=0,
-    max_tokens=200,
-    timeout=None,
-    max_retries=2,
-)
+# openai_llm = ChatOpenAI(model="gpt-4o-mini",temperature=0,max_tokens=200,timeout=None,max_retries=2)
 
-hf_llm = HuggingFacePipeline(pipeline=reader)
+# hf_llm = HuggingFacePipeline(pipeline=reader)
 
-llm = taide_llm # change this use different LLM provider
+llm = taide_llm  # change this use different LLM provider
 
 rag_pipelines = [
     "multilingual-e5",
@@ -44,8 +37,7 @@ rag_pipelines = [
 async def get_answer_multilingual_e5(query: str) -> str:
     embeddings = PineconeEmbeddings(model="multilingual-e5-large")
     index_name = "sinica-rag-test-0730-multilingual-e5-large"
-    vectorstore = PineconeVectorStore(
-        index_name=index_name, embedding=embeddings)
+    vectorstore = PineconeVectorStore(index_name=index_name, embedding=embeddings)
     docs = await asyncio.to_thread(vectorstore.similarity_search, query=query, k=1)
     chain = load_qa_chain(llm, chain_type="map_reduce")
     answer = await asyncio.to_thread(chain.run, input_documents=docs, question=query)
@@ -55,8 +47,7 @@ async def get_answer_multilingual_e5(query: str) -> str:
 async def get_answer_text_embedding_3_large(query: str) -> str:
     embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
     index_name = "sinica-rag-test-0730-text-embedding-3-large"
-    vectorstore = PineconeVectorStore(
-        index_name=index_name, embedding=embeddings)
+    vectorstore = PineconeVectorStore(index_name=index_name, embedding=embeddings)
     docs = await asyncio.to_thread(vectorstore.similarity_search, query=query, k=1)
     chain = load_qa_chain(llm, chain_type="map_reduce")
     answer = await asyncio.to_thread(chain.run, input_documents=docs, question=query)
@@ -66,8 +57,7 @@ async def get_answer_text_embedding_3_large(query: str) -> str:
 async def get_answer_text_embedding_3_small(query: str) -> str:
     embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
     index_name = "sinica-rag-test-0730-text-embedding-3-small"
-    vectorstore = PineconeVectorStore(
-        index_name=index_name, embedding=embeddings)
+    vectorstore = PineconeVectorStore(index_name=index_name, embedding=embeddings)
     docs = await asyncio.to_thread(vectorstore.similarity_search, query=query, k=1)
     chain = load_qa_chain(llm, chain_type="map_reduce")
     answer = await asyncio.to_thread(chain.run, input_documents=docs, question=query)
@@ -99,30 +89,38 @@ async def create_answer(question):
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = []
 
-    st.session_state.chat_history.append({
-        "question": question,
-        "answers": {
-            "multilingual-e5": await get_answer_multilingual_e5(question),
-            "text_embedding_3_large": await get_answer_text_embedding_3_large(question),
-            "text_embedding_3_small": await get_answer_text_embedding_3_small(question),
-            "vanilla": await get_answer_without_rag(question)
-        },
-        "message_id": len(st.session_state.chat_history),
-    })
+    st.session_state.chat_history.append(
+        {
+            "question": question,
+            "answers": {
+                "multilingual-e5": await get_answer_multilingual_e5(question),
+                "text_embedding_3_large": await get_answer_text_embedding_3_large(
+                    question
+                ),
+                "text_embedding_3_small": await get_answer_text_embedding_3_small(
+                    question
+                ),
+                "vanilla": await get_answer_without_rag(question),
+            },
+            "message_id": len(st.session_state.chat_history),
+        }
+    )
 
 
 def store_feedback(data):
     current_time = time.time()
-    current_time_readable = datetime.datetime.fromtimestamp(
-        current_time).strftime('%Y-%m-%d_%H:%M:%S')
+    current_time_readable = datetime.datetime.fromtimestamp(current_time).strftime(
+        "%Y-%m-%d_%H:%M:%S"
+    )
 
-    filepath = os.path.join(os.path.dirname(__file__),
-                            "logs", f"{current_time_readable}-log.json")
+    filepath = os.path.join(
+        os.path.dirname(__file__), "logs", f"{current_time_readable}-log.json"
+    )
     # if folder does not exist, create it
     if not os.path.exists(os.path.dirname(filepath)):
         os.makedirs(os.path.dirname(filepath))
 
-    with open(filepath, 'w', encoding='utf-8') as f:
+    with open(filepath, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
 
@@ -132,25 +130,30 @@ def fbcb():
 
         st.session_state.chat_history[message_id]["feedback"] = {}
         for pipeline in rag_pipelines:
-            if f'fb_k_{pipeline}' in st.session_state:
-                st.session_state.chat_history[message_id]["feedback"][
-                    pipeline] = st.session_state[f'fb_k_{pipeline}']
+            if f"fb_k_{pipeline}" in st.session_state:
+                st.session_state.chat_history[message_id]["feedback"][pipeline] = (
+                    st.session_state[f"fb_k_{pipeline}"]
+                )
     display_answer()
-    store_feedback(
-        st.session_state.chat_history
-    )
+    store_feedback(st.session_state.chat_history)
 
 
 async def main():
     if question := st.chat_input(placeholder="Ask your question here .... !!!!"):
         await create_answer(question)
         display_answer()
-        with st.form(f'feedback-form'):
+        with st.form(f"feedback-form"):
             st.header("Feedback")
 
             for pipeline in rag_pipelines:
                 st.write(pipeline)
-                streamlit_feedback(align="flex-start", key=f'fb_k_{pipeline}', feedback_type="faces",
-                                   optional_text_label="[Optional] Please provide an explanation",)
-            st.form_submit_button('Save feedback', on_click=fbcb)
+                streamlit_feedback(
+                    align="flex-start",
+                    key=f"fb_k_{pipeline}",
+                    feedback_type="faces",
+                    optional_text_label="[Optional] Please provide an explanation",
+                )
+            st.form_submit_button("Save feedback", on_click=fbcb)
+
+
 asyncio.run(main())
